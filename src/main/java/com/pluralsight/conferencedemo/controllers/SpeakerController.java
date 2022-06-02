a7888c3dc9b1454cd824e1891e7cff3d3f9d5174
shiwangi20 <gargshiwangi2703@gmail.com>
2022-06-02 22:20:41 +0530

    first spring boot application

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -0,0 +1,46 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakerController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "{id}")
+    public void update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        speakerRepository.saveAndFlush(speaker);
+    }
+
+}
